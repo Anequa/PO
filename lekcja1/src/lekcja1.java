d706fa14422e5e64805ed35c1f456ce4c41bc7ee
Anequa <57594153+Anequa@users.noreply.github.com>
2020-10-06 18:28:34 +0200

    Add files via upload

---
+++ lekcja1/src/lekcja1.java
@@ -0,0 +1,14 @@
+public class lekcja1 {
+        public static void main(String[] orgs){
+            System.out.println("  ///// ");
+            System.out.println(" +" +"\"\"\"\"\""+"+") ;
+            System.out.println("(| ^ ^ |)") ;
+            System.out.println("  | _ |") ;
+            System.out.println(" | ._. |") ;
+            System.out.println(" +-----+") ;
+
+
+        }
+    }
+
+ ;
